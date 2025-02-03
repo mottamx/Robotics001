b5d4881c6e9897a657112c1948ecd3106376909b
mottamx <cmottaa@gmail.com>
2025-02-03 16:01:46 -0600

    feat: Robotics library working
    Tested with Puma560 plot

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-import math
+#!/usr/bin/env python
 
-radio = float(input("Radio del círculo: "))
-perimetro = 2 * math.pi * radio
-area = math.pi * radio ** 2
-volumen = (4/3) * math.pi * radio ** 3
-print(f"Volumen de la esfera: {volumen:.2f}")
-print(f"\nCírculo de radio {radio}:")
-print(f"Perímetro: {perimetro:.2f}")
-print(f"Área: {area:.2f}")
+import roboticstoolbox as rtb
+import numpy as np
+
+robot = rtb.models.DH.Puma560()
+
+# Variables articulares
+q = [0, np.deg2rad(30), -np.deg2rad(160), 0, 0, 0]
+
+# Visualizar
+robot.plot(q, block=True, backend='pyplot')
