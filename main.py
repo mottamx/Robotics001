94d2c4c423b52310169a3dc2370effe529ce7b3e
mottamx <cmottaa@gmail.com>
2025-02-03 11:30:21 -0600

    feat: Calculo del volumen

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import math
 radio = float(input("Radio del círculo: "))
 perimetro = 2 * math.pi * radio
 area = math.pi * radio ** 2
-
+volumen = (4/3) * math.pi * radio ** 3
+print(f"Volumen de la esfera: {volumen:.2f}")
 print(f"\nCírculo de radio {radio}:")
 print(f"Perímetro: {perimetro:.2f}")
 print(f"Área: {area:.2f}")
