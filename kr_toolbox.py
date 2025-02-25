78af07e93f28781330fb7be622e4ae41249dda6e
mottamx <cmottaa@gmail.com>
2025-02-25 14:29:37 -0600

    Update kr_toolbox.py

--- kr_toolbox.py
+++ kr_toolbox.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 #Para usar el DH
 import roboticstoolbox as rtb
+np.set_printoptions(suppress=True, precision=4)
 
 robot=rtb.DHRobot(
     [
@@ -20,8 +21,32 @@ robot=rtb.DHRobot(
     ], name="Kuka KR5", base=SE3(0,0,0))
 print(robot)
 
-robot.qz=[0,0,0,0,0, 0]
-robot.teach(robot.qz)
+robot.qz=[np.deg2rad(0),np.deg2rad(0),np.deg2rad(0),np.deg2rad(0),np.deg2rad(0),np.deg2rad(0)]
+# robot.teach(robot.qz)
 
-T=robot.fkine(robot.qz)
-print(T)
+T_inicial = robot.fkine(robot.qz)
+print(f"Matriz de posición inicial:\n{T_inicial}")
+
+# # Crear una nueva matriz de transformación destino
+# R_down = np.array([
+#     [1, 0, 0],    # X apunta hacia adelante, dirección original
+#     [0, 1, 0],    # Y apunta hacia la derecha, dirección original
+#     [0, 0, -1]    # Z apunta hacia abajo, dirección invertida
+# ])
+#Roll 180 en X, Pitch 0 en Y, Yaw 0 en Z
+T_destino = SE3.Trans(0.848, -0.0, 0.28) * SE3(rpy2tr(180.0, 0.142, 0.0, 'deg'))
+print("Matriz T_destino:\n", T_destino, "\n")
+
+#Para mantener codo arriba
+q_inicial = np.array([0, np.deg2rad(45), np.deg2rad(90), 0, np.deg2rad(0), 0])
+robot.plot(q_inicial, block=True)
+
+q = robot.ikine_LM(T_destino,q0=q_inicial, ilimit=100, slimit=100, tol=1e-6)
+print(f"Éxito: {q.success}")
+
+if q.success:
+    print(f"Solución de cinemática inversa: {q.q}")
+    T_resultado = robot.fkine(q.q)
+    print(f"Matriz resultante después de IK:\n{T_resultado}")
+    # Visualizar el robot en la posición calculada
+    robot.plot(q.q, block=True)
